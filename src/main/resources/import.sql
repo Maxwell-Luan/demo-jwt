e9f9ba60b92501e50e150d35b298c513cb48cf75
Luan Maxwell <luan.maxwell@HINOVA.net>
2026-02-25 12:56:15 -0300

    Bcrypt password

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$8QiFonz/PirDS3/Q5fXAwelhP7RE3MzIDBFtakD5GL4tHI6XSUzIK');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$8QiFonz/PirDS3/Q5fXAwelhP7RE3MzIDBFtakD5GL4tHI6XSUzIK');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
